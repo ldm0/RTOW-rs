48213886d29798df1cf96948d5430b20c949cd9a
Donough Liu <ldm2993593805@163.com>
2020-05-09 22:59:22 +0800

    Add lens

--- src/camera.rs
+++ src/camera.rs
@@ -1,11 +1,23 @@
 use crate::ray;
+use crate::rng;
 use crate::vec;
+use rand::Rng;
+
+fn random_in_unit_disk() -> vec::Vec3 {
+    let mut rng = rng::RNG.lock().unwrap();
+    let degree = rng.gen_range(0., 2. * std::f32::consts::PI);
+    vec::Vec3::new(degree.sin(), degree.cos(), 0.)
+}
 
 pub struct Camera {
     upper_left_corner: vec::Vec3,
     horizontal: vec::Vec3,
     vertical: vec::Vec3,
     origin: vec::Vec3,
+    // u and v are all unified
+    u: vec::Vec3,
+    v: vec::Vec3,
+    lens_radius: f32,
 }
 
 impl Camera {
@@ -15,30 +27,46 @@ impl Camera {
         upwards: vec::Vec3,
         v_aspect: f32,
         h_aspect: f32,
+        aperture: f32,
     ) -> Self {
-        let v = v_aspect / 180. * std::f32::consts::PI;
-        let h = h_aspect / 180. * std::f32::consts::PI;
+        let focus_distance = (lookat - origin).length();
+
+        let v = v_aspect * std::f32::consts::PI / 180. / 2.;
+        let h = h_aspect * std::f32::consts::PI / 180. / 2.;
 
         let (half_width, half_height) = (h.tan(), v.tan());
 
         let w = (origin - lookat).unify();
         let u = upwards.cross(w).unify();
-        let v = u.cross(w).unify();
-        println!("u:{:?} v:{:?} w:{:?} half_width:{} half_height:{}", u, v, w, half_width, half_height);
+        // Since u and v are unified, we can ensure v is unified too. (|v| = |u| x |w| * sin(90))
+        let v = u.cross(w);
+        println!(
+            "u:{:?} v:{:?} w:{:?} half_width:{} half_height:{}",
+            u, v, w, half_width, half_height
+        );
 
         let (half_vertical, half_horizontal) = (half_height * v, half_width * u);
-        let upper_left_corner = origin - w - half_vertical - half_horizontal;
-        println!("half_horizontal:{:?} half_vertical:{:?}", half_horizontal, half_vertical);
+        let upper_left_corner = origin - focus_distance * (w + half_vertical + half_horizontal);
+        println!(
+            "half_horizontal:{:?} half_vertical:{:?}",
+            half_horizontal, half_vertical
+        );
         Self {
             upper_left_corner,
-            horizontal: 2. * half_horizontal,
-            vertical: 2. * half_vertical,
+            horizontal: 2. * focus_distance * half_horizontal,
+            vertical: 2. * focus_distance * half_vertical,
             origin,
+            u,
+            v,
+            lens_radius: aperture / 2.,
         }
     }
 
     pub fn get_ray(&self, u: f32, v: f32) -> ray::Ray {
-        let direction = self.upper_left_corner + u * self.horizontal + v * self.vertical;
-        ray::Ray::new(self.origin, direction)
+        let disk = self.lens_radius * random_in_unit_disk();
+        let offset = disk.x * self.u + disk.y * self.v;
+        let origin = self.origin + offset;
+        let direction = self.upper_left_corner + u * self.horizontal + v * self.vertical - origin;
+        ray::Ray::new(origin, direction)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ mod vec;
 use rand::Rng;
 use rng::RNG;
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 800;
-const SAMPLE: u32 = 100;
-const DEPTH: u32 = 50;
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 400;
+const SAMPLE: u32 = 80;
+const DEPTH: u32 = 100;
 
 fn ray_color(ray: &ray::Ray, world: &hit::HittableList, depth: u32) -> vec::Vec3 {
     if depth == 0 {
@@ -40,29 +40,30 @@ fn main() {
 
     let l1 = material::Lambertian::new(vec::Vec3::new(0.7, 0.3, 0.3));
     let l2 = material::Lambertian::new(vec::Vec3::new(0.8, 0.8, 0.0));
-    let m1 = material::Metal::new(1.0, vec::Vec3::new(0.8, 0.6, 0.2));
+    let m1 = material::Metal::new(0.1, vec::Vec3::new(0.8, 0.6, 0.2));
     let m2 = material::Metal::new(0.3, vec::Vec3::new(0.8, 0.8, 0.8));
     let g1 = material::Glass::new(1.5);
 
-    let s1 = sphere::Sphere::new(vec::Vec3::new(0., 0., -1.), 0.5, &l1);
+    let s1 = sphere::Sphere::new(vec::Vec3::new(0., 0., -1.), 0.49, &l1);
     let s2 = sphere::Sphere::new(vec::Vec3::new(0., -100.5, -1.), 100., &l2);
-    let s3 = sphere::Sphere::new(vec::Vec3::new(-1., 0., -1.), 0.5, &m2);
-    let s4 = sphere::Sphere::new(vec::Vec3::new(1., 0., -1.), 0.5, &g1);
-    let s5 = sphere::Sphere::new(vec::Vec3::new(1., 0., -1.), -0.45, &g1);
+    let s3 = sphere::Sphere::new(vec::Vec3::new(-1., 0., -1.), 0.49, &m1);
+    let s4 = sphere::Sphere::new(vec::Vec3::new(1., 0., -1.), 0.49, &g1);
+    //let s5 = sphere::Sphere::new(vec::Vec3::new(1., 0., -1.), -0.45, &g1);
 
     let mut world = hit::HittableList::new();
     world.insert(&s1);
     world.insert(&s2);
     world.insert(&s3);
     world.insert(&s4);
-    world.insert(&s5);
+    //world.insert(&s5);
 
     let camera = camera::Camera::new(
-        vec::Vec3::new(0., 0., 0.5),
+        0.5 * vec::Vec3::new(3., 2., 1.),
         vec::Vec3::new(0., 0., -1.),
         vec::Vec3::new(0., 1., 0.),
-        60.,
-        60.,
+        90.,
+        90.,
+        0.2,
     );
 
     buffer.enumerate_rows_mut().for_each(|(y, row)| {
